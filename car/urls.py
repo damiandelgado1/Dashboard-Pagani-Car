a9cb188e393a5f3876fd314b24ba287d6e504d1e
Damian <damian.delgadogallardo01@gmail.com>
2026-05-23 11:11:51 -0400

    Define method to Manage Car in the Dashboard

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from .views import ListCar, DetailCar, CreateCar, ModifyCar, DeleteCar
+
+
+app_name = "car"
 
 urlpatterns = [
+    path('list/', ListCar.as_view(), name="list_car"),
+    path('detail/', DetailCar.as_view(), name="detail_car"),
+    path('create', CreateCar.as_view(), name="create_car"),
+    path('modify/', ModifyCar.as_view(), name="modify_car"),
+    path('delete/', DeleteCar.as_view(), name="delete_car"),
     path('admin/', admin.site.urls),
 ]
--- car/views.py
+++ car/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Car
 
-# Create your views here.
+
+# Display all Car availability
+class ListCar(ListView):
+    model = Car
+    template_name = ""
+    context_object_name = "cars"
+
+
+# Show all detail by a Car
+class DetailCar(DetailView):
+    model = Car
+    template_name = ""
+    context_object_name = "car"
+
+
+# Create new Car for Sale
+class CreateCar(CreateView):
+    model = Car
+    fields = [
+        "brand",
+        "model",
+        "motor",
+        "dimmenssion",
+        "weight",
+        "consumtion",
+        "external_color",
+        "internal_color",
+        "stock",
+        "availability",
+        "price"
+    ]
+    template_name = ""
+    success_url = reverse_lazy("")
+
+
+# Modify stated of the Car
+class ModifyCar(UpdateView):
+    model = Car
+    fields = [
+        "external_color",
+        "internal_color",
+        "stock",
+        "availability",
+        "price",
+    ]
+    template_name = ""
+    success_url = reverse_lazy("")
+
+
+# Delete Car by Shop
+class DeleteCar(DeleteView):
+    model = Car
+    template_name = ""
+    success_url = reverse_lazy("")
