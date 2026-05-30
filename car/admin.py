c2f11740dc825a4db13309cff2af975d04646e5b
Damian <damian.delgadogallardo01@gmail.com>
2026-05-30 15:15:17 -0400

    Add header to description different part of the Information Create Car

--- car/admin.py
+++ car/admin.py
@@ -3,6 +3,6 @@ from .models import Car
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ["brand", "model", "preview", "description", "dimmenssion", "weight", "consumtion", "external_color", "internal_color", "stock", "availability", "price"]
+    list_display = ["brand", "model", "photo", "preview", "description", "photo_interior", "tires", "max_speed", "tire", "external_color", "internal_color", "stock", "availability", "price"]
     list_filter = ["brand", "model", "stock", "price"]
     search_fields = ["model", "price"]
--- car_dashboard/urls.py
+++ car_dashboard/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from .views import dashboard
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('car/', include("car.urls", namespace="car")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
